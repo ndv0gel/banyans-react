8a38e3f44011e68b65966abbc9d6640b73f2587c
Nathan Vogel <ndvogel399@icloud.com>
2025-10-15 18:35:19 -1000

    Install

---
+++ my-app/src/app/page.tsx
@@ -0,0 +1,12 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "react-bootstrap";
+
+export default function Home() {
+  return (
+    <main>
+      <Container>
+        <h1 className="text-center">Banyans React!</h1>
+      </Container>
+    </main>
+  );
+}
